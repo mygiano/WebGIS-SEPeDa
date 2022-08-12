dc5c63234cacba728ef82c98fb5f923aebc82a20
mygiano <84497159+mygiano@users.noreply.github.com>
2022-08-12 10:36:08 +0800

    newdata

---
+++ data/Titik_Sekolahqgis2web_7.js
@@ -0,0 +1,8 @@
+var json_Titik_Sekolahqgis2web_7 = {
+"type": "FeatureCollection",
+"name": "Titik_Sekolahqgis2web_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama_Sekolah": "SMP Negeri 1 Siau Barat", "NPSN": null, "Nama_KepSek": "Drs. Tes Barangtes", "Alamat_Sekolah": "Jalan Lokong Banua", "Status_Akreditasi": 2.0, "Jumlah_Siswa": 0.0, "Jumlah_Guru": 2.0, "q2wHide_Jenjang_Pendidikan": 2.0, "FotoA": "DCIM\/sd-di-kudus-digabung.jpg", "FotoB": "DCIM\/sdn-serua-3-depok.jpg", "FotoC": "DCIM\/5f1e5eea6f857.jpeg" }, "geometry": { "type": "Point", "coordinates": [ 125.361417537712811, 2.747581198449489 ] } }
+]
+}
