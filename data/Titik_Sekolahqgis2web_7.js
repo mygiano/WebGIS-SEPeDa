986068ad8f0200a8ce4c297d072eade8c31c6ca7
mygiano <84497159+mygiano@users.noreply.github.com>
2022-08-12 09:38:41 +0800

    first

---
+++ data/Titik_Sekolahqgis2web_7.js
@@ -0,0 +1,8 @@
+var json_Titik_Sekolahqgis2web_7 = {
+"type": "FeatureCollection",
+"name": "Titik_Sekolahqgis2web_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama_Sekolah": "SMP Negeri 1 Siau Barat", "NPSN": null, "q2wHide_Jenjang_Pendidikan": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 125.361417537712811, 2.747581198449489 ] } }
+]
+}
